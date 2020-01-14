232faf517f95c037c9a5c1bdc538f7b415bde48c
girlinmay91 <girl.in.may.91@gmail.com>
2020-01-15 00:45:24 +0700

    [topic16_wait][not done]: check điều kiện pass/fail TC3_ExplicitWaitWith10SecondsTest

--- src/topic16_wait/WaitDemo.java
+++ src/topic16_wait/WaitDemo.java
@@ -1,5 +1,6 @@
 package topic16_wait;
 
+import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
@@ -14,64 +15,96 @@ import org.testng.annotations.Test;
 
 public class WaitDemo {
 	WebDriver driver;
-	
-	@Test(enabled=true)
+
+	@Test(enabled = false)
 	public void TC2_ImplicitWaitTest() {
+		System.out.println("=== Start TC2_ImplicitWaitTest ===");
+		System.out.println("Expected [Fail]: Thời gian loading là ~6s, thời gian chờ setting là 2s, check element hiển thị trên UI=> Kết quả: element không hiển thị");
 		driver.get("http://the-internet.herokuapp.com/dynamic_loading/2");
+
 		driver.findElement(By.xpath("//button[contains(text(),'Start')]")).click();
-		
-		//driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS); // Fail: run in ~6.7s
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS); // Pass: run in ~10.6s
+
+		System.out.println("Wait = " + new Date());
+		driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+		System.out.println("Wait = " + new Date());
+
 		Assert.assertTrue(driver.findElement(By.xpath("//h4[contains(text(),'Hello World!')]")).isDisplayed());
+		System.out.println("=== End TC2_ImplicitWaitTest ===");
 	}
-	
-	@Test(enabled=true) 
+
+	@Test(enabled = true)
 	public void TC3_ExplicitWaitWith3SecondsTest() {
-		WebDriverWait wait = new WebDriverWait(driver, 3); // Fail: run in ~5.5s
-		
+		WebDriverWait wait = new WebDriverWait(driver, 3);
+
+		System.out.println("=== Start TC3_ExplicitWaitWith3SecondsTest ===");
+		System.out.println("Expected [Pass]");
 		driver.get("http://the-internet.herokuapp.com/dynamic_loading/2");
+
 		driver.findElement(By.xpath("//button[contains(text(),'Start')]")).click();
-		
+
+		System.out.println("Wait = " + new Date());
 		wait.until(ExpectedConditions.invisibilityOfElementLocated(By.xpath("//h4[contains(text(),'Hello World!')]")));
-		Assert.assertTrue(driver.findElement(By.xpath("//h4[contains(text(),'Hello World!')]")).isDisplayed());
-		
+		System.out.println("Wait = " + new Date());
+
+		Assert.assertTrue(!driver.findElement(By.xpath("//h4[contains(text(),'Hello World!')]")).isDisplayed());
+		System.out.println("=== End TC3_ExplicitWaitWith3SecondsTest ===");
 	}
-	
-	@Test(enabled=true) 
-	public void TC3_ExplicitWaitWith5SecondsTest() {
-		WebDriverWait wait = new WebDriverWait(driver, 5); // Fail: run in ~5.4s
-		
+
+	@Test(enabled = false)
+	public void TC3_ExplicitWaitWith10SecondsTest() {
+		WebDriverWait wait = new WebDriverWait(driver, 5);
+
+		System.out.println("=== Start TC3_ExplicitWaitWith5SecondsTest ===");
+		System.out.println("Expected [Fail]: Thời gian loading là ~6s, thời gian chờ setting là 10s, check element không hiển thị trên UI => Kết quả: element hiển thị");
 		driver.get("http://the-internet.herokuapp.com/dynamic_loading/2");
+
 		driver.findElement(By.xpath("//button[contains(text(),'Start')]")).click();
-		
+
+		System.out.println("Wait = " + new Date());
 		wait.until(ExpectedConditions.invisibilityOfElementLocated(By.xpath("//h4[contains(text(),'Hello World!')]")));
-		Assert.assertTrue(driver.findElement(By.xpath("//h4[contains(text(),'Hello World!')]")).isDisplayed());
+		System.out.println("Wait = " + new Date());
 		
+		Assert.assertTrue(!driver.findElement(By.xpath("//h4[contains(text(),'Hello World!')]")).isDisplayed());
+		System.out.println("=== End TC3_ExplicitWaitWith5SecondsTest ===");
+
 	}
-	
-	@Test(enabled=true) 
+
+	@Test(enabled = false)
 	public void TC4_ExplicitWaitWith3SecondsTest() {
-		WebDriverWait wait = new WebDriverWait(driver, 3); // Pass: run in ~5.5s
-		
+		WebDriverWait wait = new WebDriverWait(driver, 3);
+
+		System.out.println("=== Start TC4_ExplicitWaitWith3SecondsTest ===");
+		System.out.println("Expected [Fail]: Thời gian loading là ~6s, thời gian chờ setting là 3s, check element hiển thị trên UI => Kết quả: element không hiển thị");
 		driver.get("http://the-internet.herokuapp.com/dynamic_loading/2");
+
 		driver.findElement(By.xpath("//button[contains(text(),'Start')]")).click();
-		
+
+		System.out.println("Wait = " + new Date());
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//h4[contains(text(),'Hello World!')]")));
-		Assert.assertTrue(driver.findElement(By.xpath("//h4[contains(text(),'Hello World!')]")).isDisplayed());
+		System.out.println("Wait = " + new Date());
 		
+		Assert.assertTrue(driver.findElement(By.xpath("//h4[contains(text(),'Hello World!')]")).isDisplayed());
+		System.out.println("=== End TC4_ExplicitWaitWith3SecondsTest ===");
+
 	}
-	
-	@Test(enabled=true) 
-	public void TC4_ExplicitWaitWith5SecondsTest() {
-		// WebDriverWait wait = new WebDriverWait(driver, 5); // Pass: run in ~5.5s
-		WebDriverWait wait = new WebDriverWait(driver, 2); // Pass: run in ~5.5s
-		
+
+	@Test(enabled = false)
+	public void TC4_ExplicitWaitWith10SecondsTest() {
+		WebDriverWait wait = new WebDriverWait(driver, 10);
+
+		System.out.println("=== Start TC4_ExplicitWaitWith5SecondsTest ===");
+		System.out.println("Expected [Pass]: Thời gian loading là ~6s, thời gian chờ setting là 10s, check element hiển thị trên UI => Kết quả: element hiển thị");
 		driver.get("http://the-internet.herokuapp.com/dynamic_loading/2");
+
 		driver.findElement(By.xpath("//button[contains(text(),'Start')]")).click();
-		
+
+		System.out.println("Wait = " + new Date());
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//h4[contains(text(),'Hello World!')]")));
-		Assert.assertTrue(driver.findElement(By.xpath("//h4[contains(text(),'Hello World!')]")).isDisplayed());
+		System.out.println("Wait = " + new Date());
 		
+		Assert.assertTrue(driver.findElement(By.xpath("//h4[contains(text(),'Hello World!')]")).isDisplayed());
+		System.out.println("=== End TC4_ExplicitWaitWith5SecondsTest ===");
+
 	}
 
 	@BeforeTest
